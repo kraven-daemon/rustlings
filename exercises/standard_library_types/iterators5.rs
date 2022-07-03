6f95730f27ff0ebc342025c1c049bb1a21aaf9f2
Kraven <kravendaemon@gmail.com>
2022-07-03 09:19:03 -0400

    modified:   exercises/standard_library_types/iterators3.rs modified:   exercises/standard_library_types/iterators4.rs modified:   exercises/standard_library_types/iterators5.rs

--- exercises/standard_library_types/iterators5.rs
+++ exercises/standard_library_types/iterators5.rs
@@ -33,7 +33,7 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
-    // map = { "variables1": Complete, "from_str": None, ... }
+    map.iter().filter(|&(_,b)| *b == value ).count()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -52,6 +52,7 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
+    0
 }
 
 #[cfg(test)]
