0a97b2ce795e628ffc84fe7bf2edc3eea2253eb3
Kraven <kravendaemon@gmail.com>
2022-11-27 11:53:53 -0500

    modified:   exercises/clippy/clippy1.rs modified:   exercises/clippy/clippy2.rs modified:   exercises/conversions/from_into.rs modified:   exercises/conversions/using_as.rs

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,36 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            Person::default()
+        }else{
+            let p: Vec<&str> = s.split(',').collect();
+            if p.len() != 2 {
+                return Person::default();
+            }else{
+                let name: &str = p[0];
+                let age: &str = p[1];
+                if name.len() == 0 {
+                    return Person::default();
+                }
+                if age.len() == 0 {
+                    return Person::default();
+                }else{
+                    match age.parse::<usize>() {
+                        Ok(a) => {
+                            Person{
+                                name: String::from(name),
+                                age: a
+                            }
+                        }
+                        _ => Person::default()
+                    }
+                }
+            }
+        }
     }
 }
 
